7c06bf4895cb5170d2a20ed82fdf3dd760e1d7bb
Abishek Velavan <59365332+abishekvelavan@users.noreply.github.com>
2021-06-13 12:20:58 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,1 +1,156 @@
+<?php
+//index.php
 
+$error = '';
+$name = '';
+$email = '';
+$subject = '';
+$mobile = '';
+$message = '';
+
+function clean_text($string)
+{
+ $string = trim($string);
+ $string = stripslashes($string);
+ $string = htmlspecialchars($string);
+ return $string;
+}
+
+if(isset($_POST["submit"]))
+{
+ if(empty($_POST["name"]))
+ {
+  $error .= '<p><label class="text-danger">Please Enter your Name</label></p>';
+ }
+ else
+ {
+  $name = clean_text($_POST["name"]);
+  if(!preg_match("/^[a-zA-Z ]*$/",$name))
+  {
+   $error .= '<p><label class="text-danger">Only letters and white space allowed</label></p>';
+  }
+ }
+ if(empty($_POST["email"]))
+ {
+  $error .= '<p><label class="text-danger">Please Enter your Email</label></p>';
+ }
+ else
+ {
+  $email = clean_text($_POST["email"]);
+  if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+  {
+   $error .= '<p><label class="text-danger">Invalid email format</label></p>';
+  }
+ }
+ if(empty($_POST["subject"]))
+ {
+  $error .= '<p><label class="text-danger">Subject is required</label></p>';
+ }
+ else
+ {
+  $subject = clean_text($_POST["subject"]);
+ }
+
+if(empty($_POST["mobile"]))
+ {
+  $error .= '<p><label class="text-danger">Mobile is required</label></p>';
+ }
+ else
+ {
+  $mobile = clean_text($_POST["mobile"]);
+ }
+ 
+ if(empty($_POST["message"]))
+ {
+  $error .= '<p><label class="text-danger">Message is required</label></p>';
+ }
+ else
+ {
+  $message = clean_text($_POST["message"]);
+ }
+
+ if($error == '')
+ {
+  
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "myDB";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO users (name, email, mobile, message, subject)
+VALUES ('$name', '$email', '$mobile', '$message', '$subject')";
+
+if (mysqli_query($conn, $sql)) {
+//   echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+  $error = '<label class="text-success">New record created successfully </label>';
+  $name = '';
+  $email = '';
+  $subject = '';
+  $mobile = '';
+  $message = '';
+ }
+}
+
+?>
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Real Programmer: User Register</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+ </head>
+ <body>
+  <br />
+  <div class="container">
+   <h2 align="center"> User Register</h2>
+   <br />
+   <div class="col-md-6" style="margin:0 auto; float:none;">
+    <form method="post">
+     <br />
+     <?php if(!empty($error)) {
+         echo '<div class="alert alert-success" role="alert">'.$error.'</div>';
+     }
+     ?>
+     <div class="form-group">
+      <label>Enter Name</label>
+      <input type="text" name="name" placeholder="Enter Name" class="form-control" value="<?php echo $name; ?>" />
+     </div>
+     <div class="form-group">
+      <label>Enter Email</label>
+      <input type="text" name="email" class="form-control" placeholder="Enter Email" value="<?php echo $email; ?>" />
+     </div>
+     <div class="form-group">
+      <label>Enter Subject</label>
+      <input type="text" name="subject" class="form-control" placeholder="Enter Subject" value="<?php echo $subject; ?>" />
+     </div>
+     <div class="form-group">
+      <label>Enter Mobile</label>
+      <input type="text" name="mobile" class="form-control" placeholder="Enter Mobile" value="<?php echo $mobile; ?>" />
+     </div>
+     <div class="form-group">
+      <label>Enter Message</label>
+      <textarea name="message" class="form-control" placeholder="Enter Message"><?php echo $message; ?></textarea>
+     </div>
+     <div class="form-group" align="center">
+      <input type="submit" name="submit" class="btn btn-info" value="Submit" />
+     </div>
+    </form>
+   </div>
+  </div>
+ </body>
+</html>
